d1b489415dc108f2a51781600bbdcd0e583bb20b
shpaksasha <shpaksasha0000@gmail.com>
2020-10-26 21:37:26 +0200

    project setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +17,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
