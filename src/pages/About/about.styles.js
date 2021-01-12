f596bfabd351416b25a2dea1844679d64fbc5a34
shpaksasha <shpaksasha0000@gmail.com>
2021-01-12 19:26:55 +0200

    three pages theme.breakpoints.down sm - minHeight

--- src/pages/About/about.styles.js
+++ src/pages/About/about.styles.js
@@ -8,5 +8,8 @@ export const useStyles = makeStyles(theme => ({
         padding: '3rem 6.25rem',
         margin: '0rem',
         alignItems: 'center',
+        [theme.breakpoints.down('sm')]: {
+            minHeight: '37.4vh'
+        }
     }
 }))
